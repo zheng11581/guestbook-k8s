399f81a7fa1043157fd81df4db0c969d616a8e5b
zhenghc <zhenghc@borenhospital.com>
2022-06-07 23:05:38 +0800

    local running

--- runAll.sh
+++ runAll.sh
@@ -5,11 +5,11 @@ while true; do
     read -p "Run project as java app on local environment or run on K8S?, Y=Java, N=K8S" yn
     case $yn in
         [Yy]* )
-            nohup java -jar discovery-service/target/discovery-service-1.0.jar  &
+            nohup java -Dspring.profiles.active=local -jar discovery-service/target/discovery-service.jar  &
             sleep 3s # Waits eureka start for 3 seconds.
-            nohup java -jar guestbook-service/target/guestbook-service.jar  &
-            nohup java -jar gateway-service/target/gateway-service.jar  &
-            nohup java -jar zipkin-service/target/zipkin-service-1.0.jar  &
+            nohup java -Dspring.profiles.active=local -jar guestbook-service/target/guestbook-service.jar  &
+            nohup java -Dspring.profiles.active=local -jar gateway-service/target/gateway-service.jar  &
+            nohup java -jar zipkin-service/target/zipkin-service.jar  &
          break;;
         [Nn]* )
             kubectl delete -f kube-deploy/discovery.yaml
